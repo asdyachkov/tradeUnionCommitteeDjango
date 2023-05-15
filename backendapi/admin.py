daad8c6ce8a0a147a40c07384f8874f13843d516
Stepan <stepanderdichok@gmail.com>
2023-05-15 12:57:12 +0300

    add models, configure admin

--- backendapi/admin.py
+++ backendapi/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+from .models import News, Pins, Documents, Benefits
 
-# Register your models here.
+
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ("id", "theme", "created_at")
+    list_display_links = ("id", "theme",)
+    search_fields = ("id", "theme", "created_at")
+
+
+class PinsAdmin(admin.ModelAdmin):
+    list_display = ("id", "theme", "get_image")
+    list_display_links = ("id", "theme", "get_image")
+    search_fields = ("id", "theme", "get_image")
+    readonly_fields = ("get_image", )
+
+    def get_image(self, obj):
+        return mark_safe(f"<img src={obj.image_link} width='50' height='50'")
+
+    get_image.short_description = "Изображение пина"
+
+
+class DocumentsAdmin(admin.ModelAdmin):
+    list_display = ("id", "theme", "description")
+    list_display_links = ("id", "theme",)
+    search_fields = ("id", "theme", "description")
+
+
+class BenefitsAdmin(admin.ModelAdmin):
+    list_display = ("id", "theme", "description")
+    list_display_links = ("id", "theme",)
+    search_fields = ("id", "theme", "description")
+
+
+admin.site.register(News, NewsAdmin)
+admin.site.register(Pins, PinsAdmin)
+admin.site.register(Documents, DocumentsAdmin)
+admin.site.register(Benefits, BenefitsAdmin)
--- backendapi/models.py
+++ backendapi/models.py
@@ -1,3 +1,70 @@
+import django
 from django.db import models
 
-# Create your models here.
+
+class News(models.Model):
+
+    theme = models.CharField(null=False, max_length=200, verbose_name="Тема новости")
+    body = models.TextField(null=False, verbose_name="Текст новости")
+    created_at = models.DateTimeField(
+        null=False,
+        default=django.utils.timezone.now,
+        verbose_name="Время создания новости",
+    )
+
+    class Meta:
+        verbose_name = "Новость"
+        verbose_name_plural = "Новости"
+        db_table = "news"
+        ordering = ["id", "created_at"]
+
+    def __str__(self):
+        return str(self.theme)
+
+
+class Pins(models.Model):
+
+    theme = models.CharField(null=False, max_length=200, verbose_name="Тема пина")
+    description = models.TextField(null=False, verbose_name="Описание пина")
+    image_link = models.TextField(null=False, verbose_name="Ссылка на изображение пина")
+
+    class Meta:
+        verbose_name = "Пин"
+        verbose_name_plural = "Пины"
+        db_table = "pins"
+        ordering = ["id", "theme"]
+
+    def __str__(self):
+        return str(self.theme)
+
+
+class Documents(models.Model):
+
+    theme = models.CharField(null=False, max_length=200, verbose_name="Тема документа")
+    description = models.TextField(null=False, verbose_name="Описание документа")
+    admin_upload = models.FileField(upload_to='media', null=False, verbose_name="Файл документа")
+
+    class Meta:
+        verbose_name = "Документ"
+        verbose_name_plural = "Документы"
+        db_table = "documents"
+        ordering = ["id", "theme"]
+
+    def __str__(self):
+        return str(self.theme)
+
+
+class Benefits(models.Model):
+
+    theme = models.CharField(null=False, max_length=200, verbose_name="Тема льготы")
+    description = models.CharField(null=False, max_length=200, verbose_name="Описание льготы")
+    body = models.TextField(null=False, verbose_name="Текст льготы")
+
+    class Meta:
+        verbose_name = "Льгота"
+        verbose_name_plural = "Льготы"
+        db_table = "benefits"
+        ordering = ["id", "theme"]
+
+    def __str__(self):
+        return str(self.theme)
